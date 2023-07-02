1818a37401594cecad13199f3f3dee802083488d
gato001 <102332233+gato001k1@users.noreply.github.com>
2023-07-02 00:23:52 -0400

    windows activator
    DONT PAY THE GREEDY MICROSOFT

---
+++ windows activate.bat
@@ -0,0 +1,129 @@
+@echo off
+cls
+echo Windows Home: 1
+echo Windows Home N: 2
+echo Windows Home Single Language: 3
+echo Windows Home Country Specific: 4
+echo Windows Professional: 5
+echo Windows Professional N: 6
+echo Windows Education: 7
+echo Windows Education N: 8
+echo Windows Enterprise: 9
+echo Windows Enterprise N: 10
+set /p var= selecione un windows de el 1 al 10]:
+if "%var%"=="1" goto 1wa
+if "%var%"=="2" goto 2wa
+if "%var%"=="3" goto 3wa
+if "%var%"=="4" goto 4wa
+if "%var%"=="5" goto 5wa
+if "%var%"=="6" goto 6wa
+if "%var%"=="7" goto 7wa
+if "%var%"=="8" goto 8wa
+if "%var%"=="9" goto 9wa
+if "%var%"=="10" goto 10wa
+echo has seleccionadu una opcion invalida
+pause
+cls
+goto op20
+
+:1wa
+echo activando windows home
+slmgr/ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q9
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:2wa
+echo activando windows home N
+slmgr/ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:3wa
+echo activando Windows Home Single Language
+slmgr/ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:4wa
+echo activando Windows Home Country Specific
+slmgr/ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:5wa
+echo activando Windows Professional
+slmgr/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:6wa
+echo activando Windows Professional N
+slmgr/ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:7wa
+echo activando Windows Education
+slmgr/ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+
+:8wa
+echo activando Windows Education N
+slmgr/ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+
+:9wa
+echo activando Windows Enterprise
+slmgr/ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
+
+:10wa
+echo activando Windows Enterprise N
+slmgr/ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+slmgr/skms kms8.msguides.com
+slmgr/ato
+echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+pause
+cls
+goto inicio
