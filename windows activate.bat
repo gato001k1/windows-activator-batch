93acc92efa4148330815f9814b05901fe9c6b96c
gato001 <102332233+gato001k1@users.noreply.github.com>
2024-01-05 21:07:03 -0500

    Update windows activate.bat
    I am lazy

--- windows activate.bat
+++ windows activate.bat
@@ -10,7 +10,7 @@ echo Windows Education: 7
 echo Windows Education N: 8
 echo Windows Enterprise: 9
 echo Windows Enterprise N: 10
-set /p var= selecione un windows de el 1 al 10]:
+set /p var= select a windows version 1-10]:
 if "%var%"=="1" goto 1wa
 if "%var%"=="2" goto 2wa
 if "%var%"=="3" goto 3wa
@@ -21,7 +21,7 @@ if "%var%"=="7" goto 7wa
 if "%var%"=="8" goto 8wa
 if "%var%"=="9" goto 9wa
 if "%var%"=="10" goto 10wa
-echo has seleccionadu una opcion invalida
+echo invalid option
 pause
 cls
 goto op20
@@ -31,7 +31,7 @@ echo activando windows home
 slmgr/ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q9
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -41,7 +41,7 @@ echo activando windows home N
 slmgr/ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -51,7 +51,7 @@ echo activando Windows Home Single Language
 slmgr/ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -61,7 +61,7 @@ echo activando Windows Home Country Specific
 slmgr/ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -71,7 +71,7 @@ echo activando Windows Professional
 slmgr/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -81,7 +81,7 @@ echo activando Windows Professional N
 slmgr/ipk MH37W-N47XK-V7XM9-C7227-GCQG9
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -91,7 +91,7 @@ echo activando Windows Education
 slmgr/ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -102,7 +102,7 @@ echo activando Windows Education N
 slmgr/ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -113,7 +113,7 @@ echo activando Windows Enterprise
 slmgr/ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
@@ -123,7 +123,7 @@ echo activando Windows Enterprise N
 slmgr/ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
 slmgr/skms kms8.msguides.com
 slmgr/ato
-echo si no se ha activado windows es que tienes que ejecutar cathacktools en administrador
+echo if not activated execute as admin
 pause
 cls
 goto inicio
